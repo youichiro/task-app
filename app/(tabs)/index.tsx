24a294c1264a6ed1db11df3237ef1ae46e642e5d
youichiro <cinnamon416@gmail.com>
2025-04-20 10:21:12 +0900

    gluestack-uiのInputを表示してみる

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
+import { Input, InputField } from '@/components/ui/input';
 import { Text, View } from 'react-native';
 
 export default function TabOneScreen() {
   return (
     <View>
       <Text>Tab One</Text>
+      <Text>Email</Text>
+      <Input>
+        <InputField type="text" placeholder="Email" />
+      </Input>
     </View>
   );
 }
