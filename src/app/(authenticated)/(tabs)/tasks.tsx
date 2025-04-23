c2c55fe10e9b02fb073c7cf4280625e9d10b9326
youichiro <cinnamon416@gmail.com>
2025-04-23 22:18:39 +0900

    bottom drawerを表示する

--- src/app/(authenticated)/(tabs)/tasks.tsx
+++ src/app/(authenticated)/(tabs)/tasks.tsx
@@ -1,10 +1,14 @@
 import { Card } from '@/components/ui/card';
 import { Checkbox, CheckboxIcon, CheckboxIndicator, CheckboxLabel } from '@/components/ui/checkbox';
-import { CheckIcon } from '@/components/ui/icon';
+import { Drawer, DrawerBackdrop, DrawerBody, DrawerContent } from '@/components/ui/drawer';
+import { Fab, FabIcon } from '@/components/ui/fab';
+import { AddIcon, CheckIcon } from '@/components/ui/icon';
+import { Text } from '@/components/ui/text';
 import { useGlobalSession } from '@/hooks/useGlobalSession';
 import { useTasks } from '@/hooks/useTasks';
 import type { Task } from '@/hooks/useTasks';
 import BasicLayout from '@/layouts/basicLayout';
+import { useState } from 'react';
 import { View } from 'react-native';
 
 export default function TasksScreen() {
@@ -12,6 +16,7 @@ export default function TasksScreen() {
   if (!session || !session.user) return;
 
   const { tasks } = useTasks({ userId: session.user.id });
+  const [showDrawer, setShowDrawer] = useState(false);
 
   const TaskItem = ({ task }: { task: Task }) => (
     <View className="p-2">
@@ -19,7 +24,9 @@ export default function TasksScreen() {
         <CheckboxIndicator>
           <CheckboxIcon as={CheckIcon} />
         </CheckboxIndicator>
-        <CheckboxLabel size="sm" className="text-primary font-bold">{task.title}</CheckboxLabel>
+        <CheckboxLabel size="sm" className="text-primary font-bold">
+          {task.title}
+        </CheckboxLabel>
       </Checkbox>
     </View>
   );
@@ -31,6 +38,17 @@ export default function TasksScreen() {
           <TaskItem key={task.id} task={task} />
         ))}
       </Card>
+      <Fab onPress={() => setShowDrawer(true)}>
+        <FabIcon as={AddIcon} size="lg" />
+      </Fab>
+      <Drawer anchor='bottom' isOpen={showDrawer} onClose={() => setShowDrawer(false)}>
+        <DrawerBackdrop />
+        <DrawerContent>
+          <DrawerBody>
+            <Text>aaa</Text>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </BasicLayout>
   );
 }
